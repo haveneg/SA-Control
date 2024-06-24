ee6e9a2930fa54d0f4531428f69cc8285236347f
haveneg <havenedward24@gmail.com>
2024-06-24 16:23:30 -0400

    Fully functional. links.json file obsolete.

--- windows.js
+++ windows.js
@@ -1,37 +1,98 @@
+/*
+Structure of each added element or  gizmo
+
+<div class="iframe-container">
+            <h2>Title</h2>
+            <iframe src="hall-lights.html" width="100%" height="200"></iframe>
+            <div class="button-container">
+                <button id="hallLightsOn">ON</button>
+                <button id="hallLightsOff">OFF</button>
+            </div>
+        </div>
+*/
 
 
+//globals
+parent = document.getElementById('gizmo-container')
+
+gizmoArray = [];
+
+
+//Gizmo class
+class Gizmo {
+    constructor(num=0, title="anon",deflt='none',on='none',off='none') {
+        this.num = num;
+        this.title = title;
+        this.default = deflt;
+        this.on = on;
+        this.off = off;
+
+        //html constructing
+        this.container = document.createElement('div');
+        this.container.class = "iframe-container";
+        parent.appendChild(this.container);
+
+        //title
+        this.label = document.createElement('h2');
+        this.label.textContent = this.title;
+        this.container.appendChild(this.label);
+
+        //iframe
+        this.iframe = document.createElement("iframe");
+        this.iframe.width = "75%";
+        this.iframe.height = "200";
+        this.iframe.src = this.default;
+        this.container.appendChild(this.iframe);
+
+        //button container
+        this.buttonContainer = document.createElement('div');
+        this.buttonContainer.class = 'button-container';
+        this.container.appendChild(this.buttonContainer);
+
+        // buttons
+        this.onButton = document.createElement("button");
+        this.onButton.id = `${this.num}ON`;
+        this.onButton.textContent = "TRIGGER";
+        this.onButton.addEventListener('click', () => handleClick(this.num, 'ON'));
+        this.buttonContainer.appendChild(this.onButton);
+        this.offButton = document.createElement("button");
+        this.offButton.id = `${this.num}OFF`;
+        this.offButton.textContent = "RESET";
+        this.offButton.addEventListener('click', () => handleClick(this.num, 'OFF'));
+        this.buttonContainer.appendChild(this.offButton);
+    }
+}
 
 // Function to handle button clicks
-function handleClick(action, iframeId) {
-    const iframe = document.getElementById(iframeId);
-    console.log(action);
+function handleClick(ind, action) {
+    if (action == 'ON') {
+        gizmoArray[ind].iframe.src = gizmoArray[ind].on;
+    } else if (action == 'OFF') {
+        gizmoArray[ind].iframe.src = gizmoArray[ind].off;  
+    } else {
+        console.error(`The input ${action} is not recognized for buttons`);
+    }
 }
 
 // Function to handle reset all button click
 function handleResetAll() {
-    const iframes = document.querySelectorAll('iframe');
-    iframes.forEach(iframe => {
-        iframe.contentWindow.postMessage('reset', '*');
+    gizmoArray.forEach(obj => {
+        obj.iframe.src = obj.default;
     });
 }
 
-document.addEventListener('load', 'main');
+document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    const data = fetch("./links.json").then().then().catch();
-    console.log(data);
-    console.log("The main function works");
+    jsonData = JSON.parse(document.getElementById("json-data").innerText);
+
+    // Parses the json data into objects, the gizmo object then constructs html elements for each gizmo
+    for (let i = 0; i < jsonData.length; i++) {
+        obj = new Gizmo(i, jsonData[i].title, jsonData[i].default, jsonData[i].ON, jsonData[i].OFF);
+        gizmoArray.push(obj);
+    }
+    // Reset all button event
+    document.getElementById('resetAll').addEventListener('click', handleResetAll);
 }
 
-// Add event listeners to buttons
-document.getElementById('hallLightsOn').addEventListener('click', () => handleClick('on', 'hall-lights'));
-document.getElementById('hallLightsOff').addEventListener('click', () => handleClick('off', 'hall-lights'));
-document.getElementById('shoeboxOn').addEventListener('click', () => handleClick('on', 'shoebox'));
-document.getElementById('shoeboxOff').addEventListener('click', () => handleClick('off', 'shoebox'));
-document.getElementById('knockKnockOn').addEventListener('click', () => handleClick('on', 'knock-knock'));
-document.getElementById('knockKnockOff').addEventListener('click', () => handleClick('off', 'knock-knock'));
-document.getElementById('redLightOn').addEventListener('click', () => handleClick('on', 'red-light'));
-document.getElementById('redLightOff').addEventListener('click', () => handleClick('off', 'red-light'));
-document.getElementById('discoLightsOn').addEventListener('click', () => handleClick('on', 'disco-lights'));
-document.getElementById('discoLightsOff').addEventListener('click', () => handleClick('off', 'disco-lights'));
-document.getElementById('resetAll').addEventListener('click', handleResetAll);
+
